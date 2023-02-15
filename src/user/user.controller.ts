54d2ba80d0036ff8cff7f47e36ff0a67f2c09f57
MikhailKolomoitsev <75487015+MikhailKolomoitsev@users.noreply.github.com>
2023-02-15 17:12:56 +0700

    test

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,16 +2,22 @@ import * as common from "@nestjs/common";
 import * as swagger from "@nestjs/swagger";
 import * as nestAccessControl from "nest-access-control";
 import { UserService } from "./user.service";
-import { UserControllerBase } from "./base/user.controller.base";
+import { Body, Controller, Post } from "@nestjs/common";
+import { UserCreateInput } from "./base/UserCreateInput";
+import { Prisma } from "@prisma/client";
 
 @swagger.ApiTags("users")
-@common.Controller("users")
-export class UserController extends UserControllerBase {
+@Controller("users")
+export class UserController {
   constructor(
     protected readonly service: UserService,
     @nestAccessControl.InjectRolesBuilder()
     protected readonly rolesBuilder: nestAccessControl.RolesBuilder
-  ) {
-    super(service, rolesBuilder);
+  ) {}
+
+  @Post("")
+  createUser(@Body() dto: Prisma.UserCreateArgs) {
+    console.log('Post')
+    return this.service.create(dto);
   }
 }
